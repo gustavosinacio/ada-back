52291813e9198e9c5694c3827fb562ce71647ece
Gustavo Inacio <gsinacio94@gmail.com>
2021-08-15 22:12:42 -0300

    adds database configuration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 
-import swaggerSetup from './swagger.json';
 import { router } from './routes';
+import swaggerSetup from './swagger.json';
+
+import './database';
 
 const app = express();
 
